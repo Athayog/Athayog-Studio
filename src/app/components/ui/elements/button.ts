8e057d1a9c7609b0502e2d239738a0f277279782
HarsimranBarki <harsimransinghbarki@gmail.com>
2024-09-09 23:31:17 +0530

    added theme

--- src/app/components/ui/elements/button.ts
+++ src/app/components/ui/elements/button.ts
@@ -1,11 +1,17 @@
+import { Theme } from '@mui/material';
+
 export const button = {
     MuiButton: {
         styleOverrides: {
-            root: {
+            root: ({ theme }: { theme: Theme }) => ({
                 textTransform: 'none',
                 borderRadius: '1.25rem',
                 variants: [], // Add this property
-            },
+                '&:hover': {
+                    backgroundColor: theme.palette.primary.main,
+                    color: 'white',
+                },
+            }),
         },
     },
 };
